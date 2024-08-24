6f0687cc53c4b5f2e6a875fe1184baf6bdc3d42e
Eric Junior <eric.faria2003@gmail.com>
2024-08-24 11:16:49 -0300

    Feito exercicios 3 e 4

---
+++ Exercicio3.py
@@ -0,0 +1,9 @@
+num1 = int(input("Insira o primeiro número: \n"))
+num2 = int(input("Insira o segundo número: \n"))
+
+if num1 == num2:
+    C = num1 + num2
+    print("O valor é ",C)
+elif num1 != num2:
+    C = num1 * num2
+    print("O valor é ",C)
---
+++ Exercicio4.py
@@ -0,0 +1,7 @@
+num = int(input("Insira o número para descobrir seu sucessor e antecessor: \n"))
+
+
+antecessor = num -1
+sucessor = num +1
+
+print("O antecessor é ",antecessor, "O sucessor é", sucessor)
