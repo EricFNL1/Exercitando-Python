2357a2104fd988870e4b1a1baa433a889f9c76d9
Eric Notebook <eric.faria2003@gmail.com>
2024-08-26 22:14:44 -0300

    Feito Exercicio 22

---
+++ Exercicio22.py
@@ -0,0 +1,11 @@
+num1 = int(input("Insira o num 1: "))
+divisor = int(input("Insira o num 2: "))
+
+
+divisao = num1/divisor
+
+rest1 = num1 % divisor
+
+print("Quociente",divisao, "Resto: ", rest1)
+
+
